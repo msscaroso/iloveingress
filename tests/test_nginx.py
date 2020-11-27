9f4c74c29b51200887bc7e39a00ffd7dbd1deb26
Marcio Caroso <msscaroso@gmail.com>
2020-11-27 17:17:14 -0300

    Add service to create nginx locations

---
+++ tests/test_nginx.py
@@ -0,0 +1,32 @@
+from services.nginx import BaseConfig
+from tests.common import load_file_from_resources
+
+
+class TestGenerateConfigFile(object):
+
+    def test_initial_config_is_sample_config(self):
+        expected_conf = load_file_from_resources("nginx/expected.conf")
+        nginx_conf = BaseConfig()
+        assert nginx_conf.as_str() == expected_conf
+
+    def test_add_location(self):
+        expected_conf = load_file_from_resources("nginx/location.conf")
+        nginx_conf = BaseConfig()
+        nginx_conf.add_location(
+            path="/",
+            to="http://example.com"
+        )
+        assert nginx_conf.as_str() == expected_conf
+
+    def test_add_multiple_locations(self):
+        expected_conf = load_file_from_resources("nginx/multiple_locations.conf")
+        nginx_conf = BaseConfig()
+        nginx_conf.add_location(
+            path="/",
+            to="http://example.com"
+        )
+        nginx_conf.add_location(
+            path="/test",
+            to="http://another.com"
+        )
+        assert nginx_conf.as_str() == expected_conf
