9e3c28a5a2d854a392eede7e1e4a7172255ef018
Marcio Caroso <msscaroso@gmail.com>
2020-11-27 20:50:37 -0300

    refactor

--- services/ingress.py
+++ services/ingress.py
@@ -1,18 +1,29 @@
 from services.nginx import BaseConfig
 
 
-def translate_rules_to_nginx_conf(rules):
-    nginx = BaseConfig()
+def translate_rules_to_locations(rules):
+    locations = []
     for rule in rules:
         server_name = rule.get("host", "")
         for path in rule["http"]["paths"]:
             service_name = path['backend']['service_name']
             port = path['backend']['service_port']
             to = f"http://{service_name}:{port}"
-            nginx.add_location(
-                server_name=server_name,
-                path=path["path"],
-                to=to
+            locations.append(
+                {
+                    "server_name": server_name,
+                    "path": path["path"],
+                    "to": to
+                }
             )
+    return locations
 
+
+def translate_rules_to_nginx_conf(rules):
+    nginx = BaseConfig()
+    locations = translate_rules_to_locations(rules)
+    for location in locations:
+        nginx.add_location(
+            **location
+        )
     return nginx.as_str()
